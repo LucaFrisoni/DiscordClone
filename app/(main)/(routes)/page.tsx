b44ee46200854a354280187adb2633d4a52eba0e
LucaFrisoni <frisoniluca1@gmail.com>
2023-09-09 15:43:26 -0300

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toogle";
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+   <div>
+    <UserButton 
+    afterSignOutUrl="/"/>
+    <ModeToggle />
+   </div>
+  )
+}
